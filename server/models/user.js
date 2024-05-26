e3edd7c343bb6b6fb2cbcb7d9167e75b80022850
Tushar Balakrishnan <tusharbala284@gmail.com>
2024-05-26 13:10:34 +0530

    Generated jwt token

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const userSchema = new Schema({
     email: {
@@ -10,10 +12,19 @@ const userSchema = new Schema({
     password: {
         type: String,
         trim: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
 }, {
     timestamps: true
 })
+
+userSchema.methods.generateAuthToken = () => {
+    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, process.env.JWT_SECRET_KEY)
+    return token
+}
 
 
 const User = mongoose.model('User', userSchema);
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
+// Register a new user 
 router.post('/register', async (req, res) => {
     try {
         const { email, password } = req.body
@@ -22,10 +25,42 @@ router.post('/register', async (req, res) => {
 
         await newUser.save()
 
-        return res.status(201).send({ message: 'New user created successfully' })
+        return res.status(201).send({ message: 'New user created successfully', user: newUser })
 
     } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Error while registering user' })
+    }
+})
 
+
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email });
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Invalid email or password' })
+        }
+        if (!user) {
+            return res.status(400).json({ message: 'User not found, kindly register' })
+        }
+
+        const checkPassword = bcrypt.compareSync(password, user.password);
+        if (!checkPassword) {
+            return res.status(400).json({ message: 'Incorrect password' })
+        }
+
+        const token = user.generateAuthToken()
+        const data = {
+            token: token,
+            isAdmin: user.isAdmin,
+            _id: user._id
+        }
+        return res.status(200).json({ message: 'User logged in successfully', data })
+
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Error while logging in user' })
     }
 })
 
