6c66bcb81cdcd61115ed519a4ebf7114b951fe0c
Tushar Balakrishnan <tusharbala284@gmail.com>
2024-05-27 14:40:20 +0530

    product page tweaks

--- client/src/Pages/User/ProductPage.jsx
+++ client/src/Pages/User/ProductPage.jsx
@@ -13,17 +13,20 @@ const ProductPage = () => {
         setProduct(response.data.product);
     }
     return (
-        <div className="card mb-3" style={{ maxWidth: 540 }}>
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="..." className="img-fluid rounded-start" alt="..." />
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">{product.name}</h5>
-                        <p className="card-text">{product.description}</p>
-                        <p className="card-text"><small className="text-body-secondary">${product.price}</small></p>
-                        <p className="card-text text-muted"><small className="text-body-secondary">{product.category}</small></p>
+        <div className="container">
+            <div className="card mt-5 mb-3">
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img src="..." className="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{product.name}</h5>
+                            <p className="card-text">{product.description}</p>
+                            <p className="card-text"><small className="text-body-secondary">${product.price}</small></p>
+                            <p className="card-text text-muted"><small className="text-body-secondary">{product.category}</small></p>
+                            <button type="button" class="btn btn-primary">Add To Cart</button>
+                        </div>
                     </div>
                 </div>
             </div>
